ac5846dfc28e4be26816e7a7eb5de8c539265f8c
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2026-01-16 22:26:55 +0300

    Migration to TS & Prettier Reformatting

---
+++ src/config/Interfaces.ts
@@ -0,0 +1,1 @@
+export type ThemeType = { theme: string; toggleTheme: () => void };
---
+++ src/utils/ThemeContext.tsx
@@ -0,0 +1,57 @@
+import Cookies from "js-cookie";
+import {
+  createContext,
+  useState,
+  useEffect,
+  ReactNode,
+  useContext,
+} from "react";
+import { ThemeType } from "../config/Interfaces";
+
+export const ThemeContext = createContext<ThemeType | undefined>(undefined);
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const themeGet = Cookies.get("theme");
+
+    if (!themeGet) {
+      const initialTheme = prefersDark ? "dark" : "light";
+      Cookies.set("theme", initialTheme, { expires: 365 });
+      setTheme(initialTheme);
+    } else {
+      setTheme(themeGet);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+
+    Cookies.set("theme", newTheme, { expires: 365 });
+    setTheme(newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (!context)
+    throw new Error("ThemeContext must be used within ThemeProvider");
+
+  return context;
+}
+
+export default ThemeProvider;
